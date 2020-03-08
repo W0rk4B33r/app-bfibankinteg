7bc5eacd62a3eb9bc1d4b7de0b50935d4ce4549f
Chester Arellano <chester.arellano@apptech-experts.com>
2020-03-09 00:38:40 +0800

    Update Login.controller.js

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -214,6 +214,17 @@ sap.ui.define([
             url: "https://18.136.35.41:4300/app_xsjs/ExecQuery.xsjs?dbName=SBODEMOAU_SL&procName=spAppBankIntegration&QUERYTAG="+ queryTag +
             "&VALUE1=&VALUE2=&VALUE3=&VALUE4=",
             type: "GET",
+            crossDomain: true,
+            dataType:"json",
+            // xhrFields: {
+            //   withCredentials: true
+            // },
+            headers : {
+              "Access-Control-Allow-Origin" : "*"
+            },
+            beforeSend: function(xhr) {
+              xhr.setRequestHeader("Authorization", "Basic " + btoa("SYSTEM:P@ssw0rd805~"));
+            },
             xhrFields: {
               withCredentials: true
             },

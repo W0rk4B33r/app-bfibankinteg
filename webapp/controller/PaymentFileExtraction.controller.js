df931bc09c64ae46475c8592338fc0832870bc39
W0rk4B33r <78725114+W0rk4B33r@users.noreply.github.com>
2021-04-20 17:15:19 +0800

    Update PaymentFileExtraction.controller.js

--- webapp/controller/PaymentFileExtraction.controller.js
+++ webapp/controller/PaymentFileExtraction.controller.js
@@ -1028,9 +1028,9 @@ sap.ui.define([
 				var iTotalCheck = 1;
 				var sPayeeName = this.oMdlAP.getData().allopenAP[d].CardName;
 				var splitAdd = this.oMdlAP.getData().allopenAP[d].Address;
-				splitAdd = splitAdd.match(/.{1,50}/g); // split string every 50 char
-				var sAddress = (splitAdd[0] === null ? "" :  splitAdd[0]);
-				var sAddress2 =  (splitAdd[1] === null ? "" :  splitAdd[1]);
+				var splitAdd2 = splitAdd.match(/.{1,50}/g); // split string every 50 char
+				var sAddress = (splitAdd2 === null || splitAdd2[0] === "null"  ? "" :  splitAdd2[0]);
+				var sAddress2 =  (splitAdd2 === null || splitAdd2[0] === "null" ? "" :  splitAdd2[1]);
 				var sTIN = (this.oMdlAP.getData().allopenAP[d].TIN === null ? "" :  this.oMdlAP.getData().allopenAP[d].TIN);
 				var sZipCode = (this.oMdlAP.getData().allopenAP[d].ZipCode === null ? "" :  this.oMdlAP.getData().allopenAP[d].ZipCode);
 				var sPayeeCode =this.oMdlAP.getData().allopenAP[d].CardCode;//results.CardCode;
